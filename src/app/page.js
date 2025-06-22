e23d33f713a276576742c1373c5b61aaee4c540b
Atharva Deo <deoatharva44@gmail.com>
2025-06-22 08:05:42 +0530

    css file

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
 "use client"
 
-import CuisineSelector from "../app/cuisine-selector"
 import TodoList from "../app/todo-list"
 
 export default function Page() {
   return (
     <div>
-      <CuisineSelector />
       <TodoList />
     </div>
   )
